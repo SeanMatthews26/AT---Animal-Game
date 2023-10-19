43578118431b8a0d91f4ee5f2224af93e9e811d5
Sean <Sean3.matthews@live.uwe.ac.uk>
2023-10-19 14:23:13 +0100

    Can catch animals with voice and mouse

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -1,44 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Animal : MonoBehaviour
 {
-    float deleteTime = 10f;
+    private float deleteTime = 10f;
+    private Vector2 mousePos;
+    private BoxCollider2D boxCollider2D;
+    private float animalSpeed = 1;
 
     // Start is called before the first frame update
     void Start()
     {
+        //Movement
         if(transform.position.y > 6.4f)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -3f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -animalSpeed);
         }
 
         if (transform.position.y < -6.4f)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 3f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, animalSpeed);
         }
 
         if (transform.position.x < -9.9f)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(3f, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(animalSpeed, 0);
         }
 
         if (transform.position.x > 9.9f)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-3f, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-animalSpeed, 0);
         }
 
-        StartCoroutine(Delete());
+        //StartCoroutine(DeleteAfterTime());
+    }
+
+    private void OnEnable()
+    {
+        boxCollider2D = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
     }
 
-    private IEnumerator Delete()
+    private IEnumerator DeleteAfterTime()
     {
         WaitForSeconds wait = new WaitForSeconds(deleteTime);
 
@@ -47,5 +57,19 @@ public class Animal : MonoBehaviour
         Destroy(gameObject);
     }
 
+    public void Catch()
+    {
+        if(boxCollider2D == null)
+        {
+            Debug.Log("NoCollider");
+            return;
+        }
+
+        if(boxCollider2D.OverlapPoint(mousePos))
+        {
+            Debug.Log("Overlap");
+            Destroy(gameObject);
+        }
+    }
 
 }
--- Assets/Scripts/SpeechRecognition.cs
+++ Assets/Scripts/SpeechRecognition.cs
@@ -2,18 +2,27 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Windows.Speech;
 
 public class SpeechRecognition : MonoBehaviour
 {
+    //Speech
     private Dictionary<string, Action> keywordActions = new Dictionary<string, Action>();
     private KeywordRecognizer keywordRecognizer;
+
+    //Prefabs
+    [SerializeField] private GameObject CowGameObject;
 
     // Start is called before the first frame update
     void Start()
     {
-        keywordActions.Add("hello", Hello);
+        keywordActions.Add("Cow", Cow);
+        keywordActions.Add("Bear", Bear);
+        keywordActions.Add("Sheep", Sheep);
+        keywordActions.Add("Moose", Moose);
 
         keywordRecognizer = new KeywordRecognizer(keywordActions.Keys.ToArray());
         keywordRecognizer.OnPhraseRecognized += OnKeywordsRecognised;
@@ -22,13 +31,38 @@ public class SpeechRecognition : MonoBehaviour
 
     private void OnKeywordsRecognised(PhraseRecognizedEventArgs args)
     {
-        Debug.Log("Keyword: " + args.text);
-        keywordActions[args.text].Invoke();
+        //Debug.Log("Keyword: " + args.text);
+        //keywordActions[args.text].Invoke();
+
+        GameObject[] animals = GameObject.FindGameObjectsWithTag(args.text);
+        foreach (GameObject animal in animals)
+        {
+            animal.GetComponent<Animal>().Catch();
+        }
     }
 
-    private void Hello()
+    private void Cow()
     {
-        //Debug.Log("Hello");
+        GameObject[] cows = GameObject.FindGameObjectsWithTag("Cow");
+        foreach (GameObject cow in cows)
+        {
+            cow.GetComponent<Animal>().Catch();
+        }
+    }
+
+    private void Bear()
+    {
+        Debug.Log("Bear");
+    }
+
+    private void Sheep()
+    {
+        Debug.Log("Sheep");
+    }
+
+    private void Moose()
+    {
+        Debug.Log("Moose");
     }
 
     // Update is called once per frame
