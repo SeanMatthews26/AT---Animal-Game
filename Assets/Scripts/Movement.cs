b8a635a2a1f81bc17234b58eb9ccc0a817342852
Sean Matthews <Sean3.Matthews@live.uwe.ac.uk>
2024-01-03 13:49:47 +0000

    Diagonal movement

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,30 +6,12 @@ public class Movement : MonoBehaviour
 {
     private float speed = 2;
     private float deleteTime = 10f;
+    private Vector2 speedVector;
 
     // Start is called before the first frame update
     void Start()
     {
-        //Movement
-        if (transform.position.y > 6.4f)
-        {
-            GetComponent<Rigidbody>().velocity = new Vector2(0, -speed);
-        }
-
-        if (transform.position.y < -6.4f)
-        {
-            GetComponent<Rigidbody>().velocity = new Vector2(0, speed);
-        }
-
-        if (transform.position.x < -9.9f)
-        {
-            GetComponent<Rigidbody>().velocity = new Vector2(speed, 0);
-        }
-
-        if (transform.position.x > 9.9f)
-        {
-            GetComponent<Rigidbody>().velocity = new Vector2(-speed, 0);
-        }
+        SetSpeed();
 
         StartCoroutine(DeleteAfterTime());
     }
@@ -38,6 +20,35 @@ public class Movement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void SetSpeed()
+    {
+        float randomFactor = Random.Range(-1f, 1f);
+
+        //Movement
+        if (transform.position.y > 6.4f)
+        {
+            speedVector = new Vector2(randomFactor, -1f);
+            GetComponent<Rigidbody>().velocity = speedVector.normalized * speed;
+        }
+
+        if (transform.position.y < -6.4f)
+        {
+            speedVector = new Vector2(randomFactor, 1);
+        }
+
+        if (transform.position.x < -9.9f)
+        {
+            speedVector = new Vector2(1, randomFactor);
+        }
+
+        if (transform.position.x > 9.9f)
+        {
+            speedVector = new Vector2(-1, randomFactor);
+        }
+
+        GetComponent<Rigidbody>().velocity = speedVector.normalized * speed;
     }
 
 
